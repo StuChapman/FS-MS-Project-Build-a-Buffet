4e0d9cccab272e50e571961e1faac60ef1603387
Stuart Chapman <chapman.stuart@sky.com>
2021-02-18 12:21:05 +0000

    Build: added category image to products.html

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +24,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
     range = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
--- products/views.py
+++ products/views.py
@@ -8,6 +8,8 @@ from .models import Product, Category
 def products(request):
     """ A view to show products """
 
+    categories = Category.objects.all()
+
     products = Product.objects.all()
 
     if request.GET:
@@ -15,11 +17,14 @@ def products(request):
             category = request.GET['category']
             products = products.filter(category__name=category)
             range = "standard"
+            image = categories.filter(name=category)
+            print(image)
 
     context = {
             'products': products,
             'category': category,
             'range': range,
+            'image': image,
         }
 
     return render(request, 'products/products.html', context)
@@ -33,12 +38,9 @@ def filter_products(request):
     if request.GET:
         if 'category_range' in request.GET:
             category_range = request.GET['category_range']
-            print(category_range)
             category_range_list = category_range.split(',')
             category = category_range_list[0]
-            print(category)
             range = category_range_list[1]
-            print(range)
 
     context = {
             'products': products,
