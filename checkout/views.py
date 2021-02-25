9837393088b9d3407898d8e0d89c964a6de15dcb
Stuart Chapman <chapman.stuart@sky.com>
2021-02-25 18:14:49 +0000

    Build: added checkout button to basket

---
+++ checkout/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from basket.contexts import basket_context
+
+# Create your views here.
+
+
+def checkout(request):
+    """ A view to return the checkout page """
+
+    """ check for a basket cookie """
+    context_items = basket_context(request)
+    basket_total = context_items['basket_total']
+    cookie_key = context_items['cookie_key']
+
+    context = {
+            'cookie_key': cookie_key,
+            'basket_total': basket_total,       
+        }
+
+    return render(request, 'checkout/checkout.html', context)
