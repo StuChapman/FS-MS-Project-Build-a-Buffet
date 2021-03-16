2385b7ce519ade1bb2da1f2872cddec79e6dd06d
Stuart Chapman <chapman.stuart@sky.com>
2021-03-16 09:08:31 +0000

    basket_basket

--- home/views.py
+++ home/views.py
@@ -8,14 +8,6 @@ from basket.contexts import basket_context
 def index(request):
     """ A view to return the index page """
 
-    """ check for a basket cookie """
-    context_items = basket_context(request)
-    basket_total = context_items['basket_total']
-    cookie_key = context_items['cookie_key']
 
-    context = {
-            'cookie_key': cookie_key,
-            'basket_total': basket_total,       
-        }
 
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/index.html')
