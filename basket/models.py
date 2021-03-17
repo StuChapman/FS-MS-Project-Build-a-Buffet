6d3fb5212de3409e95a545014537c2276ae556c4
Stuart Chapman <chapman.stuart@sky.com>
2021-03-17 16:18:03 +0000

    Fix: added id = models.AutoField(primary_key=True) to Basket model

--- basket/models.py
+++ basket/models.py
@@ -4,17 +4,16 @@ from django.db import models
 
 
 class Basket(models.Model):
+    id = models.AutoField(primary_key=True)
     cookie = models.DecimalField(max_digits=8, decimal_places=0)
-    # Credit: https://www.fullstackpython.com/django-db-models-autofield-examples.html
-    item_number = models.AutoField(verbose_name='item_number',
-                                   serialize=True,
-                                   auto_created=True,
-                                   primary_key=True)
+    item_number = models.DecimalField(max_digits=30, decimal_places=0)
     category = models.CharField(max_length=254)
     name = models.CharField(max_length=254)
     servings = models.DecimalField(max_digits=3, decimal_places=0)
     option = models.CharField(max_length=254)
     total_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
 
+    # def __str__(self):
+    #     return self.name
     def __str__(self):
-        return self.name
+        return '{} - {}'.format(self.pk, self.name)
