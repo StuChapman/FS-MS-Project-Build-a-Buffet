aac1530388774e14d8e9444b3f5eb12823797d0b
Stuart Chapman <chapman.stuart@sky.com>
2021-03-23 18:07:21 +0000

    Build: added webhook handlers

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('create_order/', views.create_order, name='create_order'),
     path('order_success/<order_number>', views.order_success, name='order_success'),
+    path('wh/', webhook, name='webhook'),
 ]
