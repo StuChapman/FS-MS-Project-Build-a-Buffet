4b56f3d38562cf310a01e94ab1773ab22f4124b3
Stuart Chapman <chapman.stuart@sky.com>
2021-03-23 10:03:31 +0000

    Build: added customer details to .post in stripe_elements

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -74,8 +74,18 @@ form.addEventListener('submit', function(ev) {
                 var postData = {
                     'csrfmiddlewaretoken': csrfToken,
                     'paymentSuccess': 'succeeded',
+                    'full_name': $.trim(form.full_name.value),
+                    'email': $.trim(form.email.value),
+                    'phone_number': $.trim(form.phone_number.value),
+                    'country': $.trim(form.country.value),
+                    'post_code': $.trim(form.postcode.value),
+                    'town_or_city': $.trim(form.town_or_city.value),
+                    'street_address1': $.trim(form.street_address1.value),
+                    'street_address2': $.trim(form.street_address2.value),
+                    'county': $.trim(form.county.value),
                 };
                 $.post(URL, postData);
+                console.log('success')
                 // form.submit();
             }
         }
