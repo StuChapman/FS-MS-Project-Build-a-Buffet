70c2781944fb5ae8c80fc582c6858028a866c6a6
Stuart Chapman <chapman.stuart@sky.com>
2021-03-31 17:15:54 +0000

    Build: started bartholemew.js

---
+++ bartholemew/static/js/bartholemew.js
@@ -0,0 +1,54 @@
+
+var bartOne = "Great! So, is the buffet for a: party, wedding, function or corporate-event?";
+var eventParty = "A party? Woohoo! How many guests will be there?";
+var eventWedding = "A wedding? Congratulations!<br>How many guests will be there?";
+var eventFunct = "I can sort that out for you!<br>How many guests will be there?";
+var eventCorporate = "Let's impress the boss!<br>How many delegates will be there?";
+
+function startBart() {
+    $('#chatbox').html(bartOne);
+    $('#startbart').hide();
+    $('#partybart').show();
+    $('#weddingbart').show();
+    $('#functionbart').show();
+    $('#corporatebart').show();
+}
+
+function eventBart(event) {
+    switch(event) {
+        case 'party':
+            $('#chatbox').html(eventParty);
+            $('#partybart').hide();
+            $('#weddingbart').hide();
+            $('#functionbart').hide();
+            $('#corporatebart').hide();
+            $('#guests').show();
+            break;
+        case 'wedding':
+            $('#chatbox').html(eventWedding);
+            $('#partybart').hide();
+            $('#weddingbart').hide();
+            $('#functionbart').hide();
+            $('#corporatebart').hide();
+            $('#guests').show();
+            break;
+        case 'funct':
+            $('#chatbox').html(eventFunct);
+            $('#partybart').hide();
+            $('#weddingbart').hide();
+            $('#functionbart').hide();
+            $('#corporatebart').hide();
+            $('#guests').show();
+            break;
+        case 'corporate':
+            $('#chatbox').html(eventCorporate);
+            $('#partybart').hide();
+            $('#weddingbart').hide();
+            $('#functionbart').hide();
+            $('#corporatebart').hide();
+            $('#guests').show();
+            break;
+        default:
+            break;
+    }
+}
