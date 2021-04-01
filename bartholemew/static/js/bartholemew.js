6efe042390e1fc456e55218c899fa38c28237bb7
Stuart Chapman <chapman.stuart@sky.com>
2021-04-01 08:00:16 +0000

    Build: added eventGuests to bartholemew.js

--- bartholemew/static/js/bartholemew.js
+++ bartholemew/static/js/bartholemew.js
@@ -1,6 +1,6 @@
 
 var bartOne = "Great! So, is the buffet for a: party, wedding, function or corporate-event?";
-var eventParty = "A party? Woohoo! How many guests will be there?";
+var eventParty = "A party? Woohoo! <br> How many guests will be there?";
 var eventWedding = "A wedding? Congratulations!<br>How many guests will be there?";
 var eventFunct = "I can sort that out for you!<br>How many guests will be there?";
 var eventCorporate = "Let's impress the boss!<br>How many delegates will be there?";
@@ -23,6 +23,7 @@ function eventBart(event) {
             $('#functionbart').hide();
             $('#corporatebart').hide();
             $('#guests').show();
+            $('#guestsbart').show();
             break;
         case 'wedding':
             $('#chatbox').html(eventWedding);
@@ -31,6 +32,7 @@ function eventBart(event) {
             $('#functionbart').hide();
             $('#corporatebart').hide();
             $('#guests').show();
+            $('#guestsbart').show();
             break;
         case 'funct':
             $('#chatbox').html(eventFunct);
@@ -39,6 +41,7 @@ function eventBart(event) {
             $('#functionbart').hide();
             $('#corporatebart').hide();
             $('#guests').show();
+            $('#guestsbart').show();
             break;
         case 'corporate':
             $('#chatbox').html(eventCorporate);
@@ -47,8 +50,19 @@ function eventBart(event) {
             $('#functionbart').hide();
             $('#corporatebart').hide();
             $('#guests').show();
+            $('#guestsbart').show();
             break;
         default:
             break;
     }
+}
+
+function guestsBart() {
+    $('#guests').hide();
+    $('#guestsbart').hide();
+    minGuests = $('#minguests').val();
+    maxGuests = $('#maxguests').val();
+    var eventGuests = "Right, so between " + minGuests + " and " + maxGuests + " guests will be attending.<br>" +
+        "Are any of them: vegan, vegetarian or pescatarian?"
+    $('#chatbox').html(eventGuests);
 }
