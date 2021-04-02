aacd271fd5d43200e64edd62993a658b131b4269
Stuart Chapman <chapman.stuart@sky.com>
2021-04-02 14:11:33 +0000

    Build: bartholemew.html summary

--- bartholemew/static/js/bartholemew.js
+++ bartholemew/static/js/bartholemew.js
@@ -4,16 +4,22 @@ var eventParty = "A party? Woohoo! <br> How many guests will be there?";
 var eventWedding = "A wedding? Congratulations!<br>How many guests will be there?";
 var eventFunct = "I can sort that out for you!<br>How many guests will be there?";
 var eventCorporate = "Let's impress the boss!<br>How many delegates will be there?";
-var guestError = "Oops! Can you check those numbers and try again please?"
-var dietBartHot = "<br>What proportion of the menu would you like to be served hot?"
-var hotFood = " hot food in this buffet, excellent.<br>Nearly finished - same question for allergy free..."
-var bartFinish = "Awesome! That's everything I need.<br>Have a check through the info you've given me then hit the Finish button below to see your recommended buffet..."
+var guestError = "Oops! Can you check those numbers and try again please?";
+var dietBartHot = "<br>What proportion of the menu would you like to be served hot?";
+var hotFood = " hot food in this buffet, excellent.<br>Nearly finished - same question for allergy free...";
+var finishBart = "Awesome! That's everything I need.<br>Have a check through the info you've given me then hit the Finish button below to see your recommended buffet...";
 
-var guestType
-var minGuests
-var maxGuests
-var hotProportion
-var allegryProportion
+var eventType;
+var guestType;
+var minGuests;
+var maxGuests;
+var veganGuests;
+var veggieGuests;
+var pescGuests;
+var betweenGuests = "";
+var andGuests = "";
+var hotProportion;
+var allegryProportion;
 
 function startBart() {
     $('#chatbox').html(bartOne);
@@ -22,34 +28,29 @@ function startBart() {
 }
 
 function eventBart(event) {
+    eventType = event
     switch(event) {
         case 'party':
-            $('#event').hide();
-            $('#guests').show();
             $('#chatbox').html(eventParty);
-            guestType = "guests"
+            guestType = "guests";
             break;
         case 'wedding':
-            $('#event').hide();
-            $('#guests').show();
             $('#chatbox').html(eventWedding);
-            guestType = "guests"
+            guestType = "guests";
             break;
         case 'funct':
-            $('#event').hide();
-            $('#guests').show();
             $('#chatbox').html(eventFunct);
-            guestType = "guests"
+            guestType = "guests";
             break;
         case 'corporate':
-            $('#event').hide();
-            $('#guests').show();
             $('#chatbox').html(eventCorporate);
-            guestType = "delegates"
+            guestType = "delegates";
             break;
         default:
             break;
     }
+    $('#event').hide();
+    $('#guests').show();
 }
 
 function guestsBart() {
@@ -61,10 +62,13 @@ function guestsBart() {
     } 
     if (parseInt(maxGuests) == parseInt(minGuests) || maxGuests == "") {
         var eventGuests = "Right, so " + minGuests + " " + guestType + " will be attending.<br>" +
-            "Are any of them: vegan, vegetarian or pescatarian?"
+            "Are any of them: vegan, vegetarian or pescatarian?";
+            maxGuests = "";
     } else {
          var eventGuests = "Right, so between " + minGuests + " and " + maxGuests + " " + guestType + " will be attending.<br>" +
-        "Are any of them: vegan, vegetarian or pescatarian?"
+        "Are any of them: vegan, vegetarian or pescatarian?";
+        betweenGuests = "between";
+        andGuests = "and";
     }
     $('#guests').hide();
     $('#diet').show();
@@ -74,15 +78,15 @@ function guestsBart() {
 function dietBart() {
     veganGuests = $('#veganGuests').val();
     veggieGuests = $('#veggieGuests').val();
-    pescGuests = $('#pescguests').val();
+    pescGuests = $('#pescGuests').val();
     if ((parseInt(veganGuests*1) + parseInt(veggieGuests*1) + parseInt(pescGuests*1)) > parseInt(minGuests*1)) {
         $('#chatbox').html(guestError);
         return;
     }
     if ((parseInt(veganGuests*1) + parseInt(veggieGuests*1) + parseInt(pescGuests*1)) == 0) {
-        var dietBart = "No problem; there will be a nice mix of food in case anyone has a preference on the day." + dietBartHot
+        var dietBart = "No problem; there will be a nice mix of food in case anyone has a preference on the day." + dietBartHot;
     } else {
-         var dietBart = "Great! I'll make sure there are plenty of different choices for everyone." + dietBartHot
+         var dietBart = "Great! I'll make sure there are plenty of different choices for everyone." + dietBartHot;
     }
     $('#hotfood').show();
     $('#diet').hide();
@@ -92,7 +96,7 @@ function dietBart() {
 function hotBart(proportion) {
     $('#hotfood').hide();
     $('#allergy').show();
-    hotProportion = proportion
+    hotProportion = proportion;
     $('#chatbox').html(hotProportion + hotFood);
 }
 
@@ -102,6 +106,18 @@ function allergyBart(proportion) {
     $('#finishbart').show();
     $('#chatsummary').show();
     $('#finalimgbox').show();
-    allegryProportion = proportion
-    $('#chatbox').html(bartFinish);
+    allegryProportion = proportion;
+    $('#chatbox').html(finishBart);
+
+    var bartSummary = "A " + eventType + "<br><br>" +
+                       "With " + betweenGuests + " " + minGuests + " " + andGuests + 
+                       " " + maxGuests + " guests." + "<br><br>" +
+                       "Dietary requirements are:" + "<br>" +
+                       "Vegan: " + parseInt(veganGuests*1) + "<br>" +
+                       "Vegetarian: " + parseInt(veggieGuests*1) + "<br>" +
+                       "Pescaterian: " + parseInt(pescGuests*1) + "<br><br>" +
+                       hotProportion + " hot food in this buffet." + "<br><br>" +
+                       "A " + allegryProportion + " proportion of allergy free food";
+    console.log(bartSummary)
+    $('#chatsummarybox').html(bartSummary);
 }
